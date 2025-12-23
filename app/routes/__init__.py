48c0cdff297c5738635b8be499ce2331cdf66b0e
MiguelAngel861 <miguelangelscott861@gmail.com>
2025-12-22 19:09:25 -0600

    nueva rama

--- app/routes/__init__.py
+++
@@ -1,2 +0,0 @@
-from .auth_routes import auth_bp
-from .main_routes import main_bp
--- app/routes/auth_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-
-auth_bp = Blueprint("auth_bp", __name__)
-
-@auth_bp.route('/')
-def index():
-    return "hola"
